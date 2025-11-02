4308bffb36f7417866e7efec7f05bb11362f4af3
nathalie <nathalie@email.com>
2025-11-02 21:05:31 +1300

    made changes to allowed hosts

--- healthtracker/viewsets.py
+++ healthtracker/viewsets.py
@@ -4,16 +4,15 @@ from healthtracker.models import Activity
 from healthtracker.serializers import ActivitySerializer
 
 class ActivityViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    #permission_classes = [IsAuthenticated]
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
 
     def get_queryset(self):
         activities = Activity.objects.filter(user=self.request.user)
-        # Return activities only for the logged-in user
         return activities
 
-
+    #
     def perform_create(self, serializer):
-        # Automatically assign the logged-in user
         serializer.save(user=self.request.user)
+        serializer.save()
