c3c0b720ffb0628e2cb9d164def2c2ccb38f9ed3
nathalie <nathalie@email.com>
2025-11-05 11:47:59 +1300

    1. created serializers.py,viewsets, tests andd views for my activity status

--- healthtracker/viewsets.py
+++ healthtracker/viewsets.py
@@ -3,14 +3,17 @@ from healthtracker.models import Activity
 from healthtracker.serializers import ActivitySerializer
 
 class ActivityViewSet(viewsets.ModelViewSet):
-    queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
+        # Only return activities belonging to the logged-in user
         return Activity.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
+        # Set default status if not provided and assign the logged-in user
+        if 'status' not in serializer.validated_data:
+            serializer.validated_data['status'] = 'planned'
         serializer.save(user=self.request.user)
 
 
